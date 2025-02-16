c785921b9b235ae99accc59f6aae4b819a858111
sickwrtn <sillo154265@gmail.com>
2025-02-16 16:43:31 +0900

    주석 추가

--- source/index.ts
+++ source/index.ts
@@ -1,3 +1,4 @@
+//모듈 import
 import * as env from "./.env/env";
 import * as tools from "./tools/functions";
 import * as requests from "./tools/requests";
@@ -6,11 +7,14 @@ import { chatroom_menus_class, dropdown_class, feed_class } from "./class/class"
 import { wrtn_api_class } from "./tools/sdk";
 import { main } from "./main";
 
+//필수 class 선언
 const wrtn = new wrtn_api_class();
 const feed = new feed_class();
 const dropdown = new dropdown_class();
 const menus = new chatroom_menus_class();
 
+
+//실제 실행
 main(feed,menus,dropdown);
 
 
