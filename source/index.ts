03c1c48e3cbf57396fd04e074e7ca3ef93360ba2
sickwrtn <sillo154265@gmail.com>
2025-02-16 18:22:46 +0900

    interfaces import 추가

--- source/index.ts
+++ source/index.ts
@@ -2,6 +2,7 @@
 import * as env from "./.env/env";
 import * as tools from "./tools/functions";
 import * as requests from "./tools/requests";
+import * as interfaces from "./interface/interfaces";
 import { debug } from "./tools/debug";
 import { chatroom_menus_class, dropdown_class, feed_class } from "./class/class";
 import { wrtn_api_class } from "./tools/sdk";
@@ -12,7 +13,6 @@ const wrtn = new wrtn_api_class();
 const feed = new feed_class();
 const dropdown = new dropdown_class();
 const menus = new chatroom_menus_class();
-
 
 //실제 실행
 main(feed,menus,dropdown);
